d84b8edc2803cb6ff2443f1a783139a909fb1b01
Amadeco <2975845+Amadeco@users.noreply.github.com>
2026-02-07 20:15:08 +0100

    Update Boolean.php

--- Model/Layer/Filter/Boolean.php
+++ Model/Layer/Filter/Boolean.php
@@ -16,15 +16,18 @@ declare(strict_types=1);
 
 namespace Amadeco\SmileCustomEntityLayeredNavigation\Model\Layer\Filter;
 
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Serialize\SerializerInterface;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Catalog\Model\Layer\Filter\Item\DataBuilder;
-use Smile\CustomEntity\Model\ResourceModel\CustomEntity\Collection;
 use Amadeco\SmileCustomEntityLayeredNavigation\Model\Layer;
 use Amadeco\SmileCustomEntityLayeredNavigation\Model\Layer\Filter\ItemFactory;
 use Amadeco\SmileCustomEntityLayeredNavigation\Model\ResourceModel\Layer\Filter\Attribute as AttributeResource;
 use Amadeco\SmileCustomEntityLayeredNavigation\Model\ResourceModel\Layer\Filter\AttributeFactory;
+use Magento\Catalog\Model\Layer\Filter\Item\DataBuilder;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Filter\StripTags;
+use Magento\Framework\Serialize\SerializerInterface;
+use Magento\Framework\Stdlib\StringUtils;
+use Magento\Store\Model\StoreManagerInterface;
+use Smile\CustomEntity\Model\ResourceModel\CustomEntity\Collection;
 
 /**
  * Boolean Filter for Custom Entity attributes
@@ -42,8 +45,8 @@ class Boolean extends AbstractFilter
      * @param Layer $layer
      * @param DataBuilder $itemDataBuilder
      * @param AttributeFactory $filterAttributeFactory
-     * @param \Magento\Framework\Stdlib\StringUtils $string
-     * @param \Magento\Framework\Filter\StripTags $tagFilter
+     * @param StringUtils $string
+     * @param StripTags $tagFilter
      * @param array $data
      */
     public function __construct(
@@ -52,15 +55,23 @@ class Boolean extends AbstractFilter
         Layer $layer,
         DataBuilder $itemDataBuilder,
         AttributeFactory $filterAttributeFactory,
-        \Magento\Framework\Stdlib\StringUtils $string,
-        \Magento\Framework\Filter\StripTags $tagFilter,
+        protected readonly StringUtils $string,
+        protected readonly StripTags $tagFilter,
         array $data = []
     ) {
         $this->_resource = $filterAttributeFactory->create();
         $this->string = $string;
-        $this->_requestVar = 'attribute';
         $this->tagFilter = $tagFilter;
-        parent::__construct($filterItemFactory, $storeManager, $layer, $itemDataBuilder, $data);
+        
+        $this->_requestVar = 'attribute';
+        
+        parent::__construct(
+            $filterItemFactory, 
+            $storeManager, 
+            $layer, 
+            $itemDataBuilder, 
+            $data
+        );
     }
 
     /**
@@ -76,11 +87,11 @@ class Boolean extends AbstractFilter
     /**
      * Apply filter to collection
      *
-     * @param \Magento\Framework\App\RequestInterface $request
+     * @param RequestInterface $request
      * @return $this
      * @throws LocalizedException
      */
-    public function apply(\Magento\Framework\App\RequestInterface $request): self
+    public function apply(RequestInterface $request): self
     {
         $filter = $request->getParam($this->getRequestVar());
 
@@ -110,6 +121,7 @@ class Boolean extends AbstractFilter
             if (is_array($option['value'])) {
                 continue;
             }
+            
             if ($this->string->strlen($option['value'])) {
                 // Check filter type
                 if ($this->getAttributeIsFilterable($attribute) == self::ATTRIBUTE_OPTIONS_ONLY_WITH_RESULTS) {
